514b20906684d368b3ec9334d1a263eb42d0b670
ritaqyang <ritaqyang@gmail.com>
2023-06-23 11:01:39 -0400

    search box component finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
@@ -38,22 +39,16 @@ class App extends Component {
   render(){
 
     const { monsters, searchField } = this.state; 
-    const { onSearchChange } = this.onSearchChange; //make code more readable 
+    const { onSearchChange } = this; //make code more readable 
     const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className='App'>
-      <input 
-        className='search-box'
-        type='search'
-        placeholder='search monsters'
-        onChange={onSearchChange}
-      />
 
-
-      <CardList monsters={filteredMonsters}/> 
+        <SearchBox onChangeHandler={ onSearchChange } placeholder='search monsters' className='search-box'/>
+        <CardList monsters={ filteredMonsters }/> 
       
       </div>
     )
